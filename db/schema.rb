4cdf0082e4e2ff2e5196a6c239015f8ee52715d7
Eugene Zorkin <eugenezorkin@gmail.com>
2021-12-08 10:57:20 +0000

    make news db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_083628) do
+ActiveRecord::Schema.define(version: 2021_12_08_105321) do
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "author"
+    t.integer "number_edits"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
