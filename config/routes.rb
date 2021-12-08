09601eda945ba63d24f0bff7343fea1f34f93f9f
Eugene Zorkin <eugenezorkin@gmail.com>
2021-12-08 19:50:08 +0000

    add field name to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :news
-  devise_for :users
+  devise_for :users, :path_prefix => 'reg'
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
