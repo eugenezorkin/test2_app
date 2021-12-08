72a88b327854288e0ac26a2a5cca096251f58e02
Eugene Zorkin <eugenezorkin@gmail.com>
2021-12-08 19:57:16 +0000

    copy devise users controller to local side

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :news
-  devise_for :users, :path_prefix => 'reg'
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
