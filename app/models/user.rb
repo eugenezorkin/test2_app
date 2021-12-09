b26bb2e843ab27cce6d02aae00d3d3adf1eb9776
Eugene Zorkin <eugenezorkin@gmail.com>
2021-12-09 14:53:29 +0000

    delete roles and assigments models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable;
   has_many :news;
-  has_many :assignments
-  has_many :roles, :through => :assignments
   enum role: [ :martial, :editor, :admin  ]
   attr_accessor :name
-  before_save { self.email = email.downcase }, :default_values
+  before_save { self.email = email.downcase } # :default_values
   validates :name, presence: true, length: { maximum: 255 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
