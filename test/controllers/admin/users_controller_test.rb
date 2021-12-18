5b1bd5a62d25ab62039fa949e5978f2f95bd9b76
Eugene Zorkin <eugenezorkin@gmail.com>
2021-12-18 20:23:54 +0000

    change paths for Show action of user controller

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -22,9 +22,6 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    my_logger ||= Logger.new("#{Rails.root}/log/my.log")
-    my_logger.info "USER ONTROLLER TEST irl" 
-      my_logger.info url_for({controller: 'admin/users', action: :show, id: @user.id})
     patch url_for({controller: 'admin/users', action: :show, id: @user.id}), params: { user: { email: "newtest@test.com", name: "new test name" } }
     assert_redirected_to url_for({controller: 'admin/users', action: :edit, id: @user.id})
   end
