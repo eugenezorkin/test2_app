8ac54a255b5e46eb7e5718f9a0937308d19ed4fe
Eugene Zorkin <eugenezorkin@gmail.com>
2021-12-19 22:53:43 +0000

    add resque logging in file

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,3 +4,5 @@ uri = URI.parse("redis://localhost:6379/")
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 
+Resque.logger.level = Logger::DEBUG
+Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
